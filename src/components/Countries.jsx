3997424e2c67d4415cc731f909c6ddd47440bab9
Norton794 <nortonsantos79@gmail.com>
2022-04-01 01:33:29 -0300

    feat: displaying the countries

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,7 +1,38 @@
-export default props => {
-    return(
-        <div className="grid">
+import styles from "../styles/Countries.module.css";
+export default (props) => {
+  return (
+    <div className={styles.grid}>
+      {props.countries &&
+        props.countries.map((country) => (
+          <div
+            style={{
+              marginTop: "25px",
+              marginBottom: "20px",
+              padding: "0px",
+              backgroundColor: "white",
+            }}
+            key={country.name.common}
+          >
+            <img
+              style={{ width: "250px", height: "150px", margin: "0px" }}
+              src={country.flags.png}
+              alt={`${country.name.common} flag`}
+            />
+            <div style={{ padding: "0 2rem" }}>
+              <h4>{country.name.common}</h4>
 
-        </div>
-    )
-}
+              <p>
+                <b>Population:</b> {country.population}
+              </p>
+              <p>
+                <b>Region:</b> {country.region}
+              </p>
+              <p>
+                <b>Capital:</b> {country.capital}
+              </p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,30 @@
 import Countries from "../components/Countries";
 import Header from "../components/Header";
 import styles from "../styles/Main.module.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const URL = "https://restcountries.com/v3.1/all";
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${URL}`)
+      .then((resp) => {
+        //console.log(JSON.stringify(resp.data[0]))
+        setCountries(resp.data);
+      })
+      .catch((err) => console.log("Error", err));
+  }, []);
   return (
     <div>
       <Header />
       <div className={styles.container}>
         <div className="form"></div>
+        <Countries countries={countries} />
       </div>
-      <Countries/>
+      
     </div>
   );
 }
