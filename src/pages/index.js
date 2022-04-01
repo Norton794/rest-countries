3b60c985b086a7e4d09689130bc62538f52ce5f1
Norton794 <nortonsantos79@gmail.com>
2022-04-01 00:27:41 -0300

    feat: Header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
-import styles from '../styles/Home.module.css'
+import Header from "../components/Header";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
+    <div>
+      <Header />
+      <div className={styles.container}>
+        
+      </div>
     </div>
-  )
+  );
 }
